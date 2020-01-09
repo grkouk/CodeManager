77abb0673369129b17e27837a9af29c81a21eac6
GrKouk <grkouk@hotmail.com>
2020-01-09 10:43:56 +0200

    Small change

--- GrKouk.CodeManager/GrKouk.CodeManager/ViewModels/NopProductDetailsPageViewModel.cs
+++ GrKouk.CodeManager/GrKouk.CodeManager/ViewModels/NopProductDetailsPageViewModel.cs
@@ -1,16 +1,19 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace GrKouk.CodeManager.ViewModels
 {
-    public class NopProductDetailsPageViewModel : BindableBase
+    public class NopProductDetailsPageViewModel : ViewModelBase
     {
-        public NopProductDetailsPageViewModel()
-        {
+        private readonly INavigationService navigationService;
 
+        public NopProductDetailsPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            this.navigationService = navigationService;
         }
     }
 }
