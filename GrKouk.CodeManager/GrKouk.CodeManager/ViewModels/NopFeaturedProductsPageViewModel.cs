95ea6b0cd224c3d32bfe1aca0c5dfc592f35f644
GrKouk <grkouk@hotmail.com>
2020-11-17 10:46:53 +0200

    Feauture Products

---
+++ GrKouk.CodeManager/GrKouk.CodeManager/ViewModels/NopFeaturedProductsPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GrKouk.CodeManager.Services;
+using Prism.Navigation;
+using Prism.Services;
+
+namespace GrKouk.CodeManager.ViewModels
+{
+    public class NopFeaturedProductsPageViewModel : ViewModelBase
+    {
+        private readonly INavigationService _navigationService;
+        private readonly IPageDialogService _dialogService;
+        private readonly IDataSource _dataSource;
+        public NopFeaturedProductsPageViewModel()
+        {
+
+        }
+    }
+}
